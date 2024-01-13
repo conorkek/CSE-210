4e1f1e16b278b124dffacbf3e0a73224e5ea2818
conorkek <kek15002@byui.edu>
2024-01-12 17:24:50 -0800

    Finished C# Prep 2, didn't do stretch challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string letterGrade = "";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congrats, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you didn't pass the class. Better luck next time!");
+        }
     }
 }
