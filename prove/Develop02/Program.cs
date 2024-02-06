7e2d6214b8c79881a3be659ff9951aaab761f69d
conorkek <kek15002@byui.edu>
2024-02-06 12:58:56 -0800

    finished up the code for the Main by using Switch and Case

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,18 +1,18 @@
 using System.IO;
+using System.IO.Enumeration;
 
 class Program
 {
     static void Main(string[] args)
     {   
         Journal journal = new();
-        PromptGenerator pgen = new();
         string userInput = "";
 
-        while (userInput != "5")
+        while (true)
         {
             // have welcome menu with options
             Console.WriteLine("Welcome to your personal Journal!");
-            Console.WriteLine("Please select one of the following choicse");
+            Console.WriteLine("Please select one of the following choices");
             // PUT Add Entry
             Console.WriteLine("1. Entry");
             // PUT Display Entries
@@ -24,32 +24,36 @@ class Program
             // PUT Quit
             Console.WriteLine("5. Quit");
             // choice = GET
-            Console.Write("What would you like to do?" );
+            Console.Write("What would you like to do? ");
             userInput = Console.ReadLine();
 
             // Use a SWITCH and CASE statment to handle the users input
 
-            if (userInput == "1")
-            {
-                pgen.GetRandomPrompt();
-                journal.AddEntry();
-            }
-
-            else if (userInput == "2")
-            {
-                journal.DisplayAllEntries();
-            }
-
-            else if (userInput == "3")
-            {
-                
-            }
-
-            else if (userInput == "4")
-            {
-                
-            }
-        
+           switch(userInput)
+           {
+                case "1":
+                    journal.AddEntry();
+                    break;
+                case "2":
+                    journal.DisplayAllEntries();
+                    break;
+                case "3":
+                    Console.Write("Enter the filename: ");
+                    string filename = Console.ReadLine();
+                    journal.Save(filename);
+                    break;
+                case "4":
+                    Console.Write("Enter the filename: ");
+                    filename = Console.ReadLine();
+                    journal.Load(filename);
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input.");
+                    break;
+           }
         }
     }
 } 
