aa31e2089fbcf98251ef29c1108e75848c893cee
conorkek <kek15002@byui.edu>
2024-03-01 16:34:15 -0800

    created BreathingActivity

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,30 @@
+public class BreathingActivity: Activity
+{
+    private const int _inhaleTime = 4;
+    private const int _exhaleTime = 6;
+
+    public BreathingActivity()
+    {
+        _name = "Breathing";
+        _description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
+        StartingMessage();
+    }
+
+    public void RunBreathingActivity()
+    {
+        DateTime startTime = DateTime.Now;
+        DateTime activityLength = startTime.AddSeconds(_duration);
+        while (startTime < activityLength)
+        {
+            Console.Write($"Breathe in...");
+            CountdownTimer(_inhaleTime);
+
+            Console.Write($"Breathe out...");
+            CountdownTimer(_exhaleTime);
+            
+            startTime = DateTime.Now;
+        }
+        Console.WriteLine();
+        EndingMessage();
+    }
+}
