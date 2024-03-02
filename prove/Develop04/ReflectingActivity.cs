54ac54a892f51efd4fd62702e8f181fd68a10d83
conorkek <kek15002@byui.edu>
2024-03-02 09:39:17 -0800

    For the stretch, I added code so that a prompt wouldn't repeat until all the prompts had been used. Then the prompts would start over again.

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -11,15 +11,15 @@ public class ReflectingActivity: Activity
     };
     private string[] _followUpQuestion = 
     {
-        "Why was this experience meaningful to you?",
-        "Have you ever done anything like this before?",
-        "How did you get started?",
-        "How did you feel when it was complete?",
-        "What made this time different than other times when you were not as successful?",
-        "What is your favorite thing about this experience?",
-        "What could you learn from this experience that applies to other situations?",
-        "What did you learn about yourself through this experience?",
-        "How can you keep this experience in mind in the future?"
+        "Why was this experience meaningful to you? ",
+        "Have you ever done anything like this before? ",
+        "How did you get started? ",
+        "How did you feel when it was complete? ",
+        "What made this time different than other times when you were not as successful? ",
+        "What is your favorite thing about this experience? ",
+        "What could you learn from this experience that applies to other situations? ",
+        "What did you learn about yourself through this experience? ",
+        "How can you keep this experience in mind in the future? "
 
     };
 
@@ -34,56 +34,76 @@ public class ReflectingActivity: Activity
         StartingMessage();
     }
 
-    public string GetRandomPrompt()
-    {
-        // Gets a random index between 0 - length prompts 
-        int index = rng.Next(_startingPrompt.Length);
-        _usedQuestions.Add(index);
-        while (_usedQuestions.Contains(index))
-        {
-            
-        }
-        
-        // Returns a prompt using the index
-        return _startingPrompt[index];
-
-
-    }
     public string GetRandomFollowUpPrompt()
     {
         // Gets a random index between 0 - length prompts 
         int index = rng.Next(_followUpQuestion.Length);
 
+        if (_usedQuestions.Count == 9)
+        {
+            _usedQuestions.Clear();
+        }
+
+        while (_usedQuestions.Contains(index))
+        {
+            index = rng.Next(_followUpQuestion.Length);
+        }
+        _usedQuestions.Add(index);
         // Returns a prompt using the index
         return _followUpQuestion[index];
+
+
+    }
+    public string GetRandomPrompt()
+    {
+        // Gets a random index between 0 - length prompts 
+        int index = rng.Next(_startingPrompt.Length);
+
+        // Returns a prompt using the index
+        return _startingPrompt[index];
     }
 
     public void DisplayPrompt()
     {
+        Console.WriteLine();
+        Console.WriteLine();
         Console.WriteLine("Consider the following prompt: \b");
+        Console.WriteLine();
+        Console.WriteLine();
         Console.WriteLine(" --- " + GetRandomPrompt() + " --- " + "\b");
-        Console.WriteLine("When you have something in mind, press 'Enter' to continue.");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.Write("When you have something in mind, press 'Enter' to continue. ");
+        Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Now ponder each of the following questions as they relate to this experience.");
+        Console.Write("You may begin in: ");
+        CountdownTimer(5);
+        Console.WriteLine();
+        Console.WriteLine();
     }
 
     public void DisplayFollowUpQuestion()
     {
-        Console.WriteLine(GetRandomFollowUpPrompt() + "\b");
-        Console.ReadLine();
+        Console.Write(GetRandomFollowUpPrompt());
+        Spinner(5);
+        Console.WriteLine();
     }
 
     public void RunReflectingActivity()
     {
         DisplayPrompt();
+        Console.Clear();
         DateTime startTime = DateTime.Now;
         DateTime activityLength = startTime.AddSeconds(_duration);
         while (startTime < activityLength)
         {
             DisplayFollowUpQuestion();
-            Spinner(_duration);
             startTime = DateTime.Now;
         }
         Console.WriteLine();
         EndingMessage();
-
+        Console.Clear();
     }
 }
