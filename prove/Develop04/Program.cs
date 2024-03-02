74820445700f2690b8700a969a43059132bdfb39
conorkek <kek15002@byui.edu>
2024-03-01 16:35:34 -0800

    Added methods and created menu on Main

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Menu Options");
+            Console.WriteLine("1. Start Breathing Activity");
+            
+            Console.WriteLine("2. Start Reflecting Activity");
+          
+            Console.WriteLine("3. Start Listing Activity");
+            
+            Console.WriteLine("4. Quit");
+
+            Console.Write("Select a choice from the menu: ");
+            string userInput = Console.ReadLine();
+
+            // Use a SWITCH and CASE statment to handle the users input
+
+           switch(userInput)
+           {
+                case "1":
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.RunBreathingActivity();
+                    break;
+                case "2":
+                    ReflectingActivity reflecting = new ReflectingActivity();
+                    reflecting.RunReflectingActivity();
+                    break;
+                case "3":
+                    ListingActivity listing = new ListingActivity();
+                    listing.RunListingActivity();
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input.");
+                    break;
+           }
+        }
     }
 }
