88874d01334ef3fb529547fbc8f5461b0cbe5bc2
conorkek <kek15002@byui.edu>
2024-03-07 22:25:48 -0800

    Created the shape, sqaure, rectangle, and circle classes. Made a list of shapes on main, added a shape of each to the list. Displayed the color and areas of each shape.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("Red", 4);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("Blue", 2, 4);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("Pink", 5);
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}. ");
+        }
     }
 }
