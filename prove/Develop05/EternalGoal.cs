f659a2f676002bee26529e01c81586b33c91a01d
conorkek <kek15002@byui.edu>
2024-03-13 00:40:36 -0700

    Created and finished the EternalGoal class

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,15 @@
+public class EternalGoal: Goal
+{
+    public EternalGoal(string name, string description, int points, bool isComplete): base (name, description, points, isComplete)
+    {}
+
+    // PUBLIC OVERRIDE RecordEvent()
+    //      PUT message
+    //      RETURN _points
+    // END
+    public override int RecordEvent()
+    {
+        Console.WriteLine("Congratulations, you have been awarded points for working on your eternal goal!");
+        return _points;
+    }
+}
