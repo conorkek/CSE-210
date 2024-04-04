aa98ef82afa0215718b902c597bb6a6f76c63e6a
conorkek <kek15002@byui.edu>
2024-04-04 16:40:41 -0700

    Created a list of Activity and then displayed each activity in the format that was given.

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new()
+        {
+            new Running("4-1-2024", 60, 1),
+            new Cycling("4-2-2024", 60, 60),
+            new Swimming("4-3-2024", 60, 20)
+        };
+
+        foreach (Activity x in activities)
+        {
+            x.GetSummary();
+        }
     }
 }
